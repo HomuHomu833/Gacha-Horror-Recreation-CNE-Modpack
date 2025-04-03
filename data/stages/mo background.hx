0a668af9ef3c6bd2982b349f82180176060e4068
Solex <95106797+SantiYea@users.noreply.github.com>
2025-04-03 04:04:09 -0300

    im watching boruto

---
+++ data/stages/mo background.hx
@@ -0,0 +1,36 @@
+function create() {
+    var moe:FlxSprite = new FlxSprite(-600, 67, Paths.images('stages/mo/moe'));
+    moe.scrollFactor.set(0.9, 0.9);
+    moe.scale.set(1.6, 1.2);
+    moe.updateHitbox();
+    
+    var mo:FlxSprite = new FlxSprite(-1000, -190, Paths.images('stages/mo/mo'));
+    mo.scrollFactor.set(0.9, 0.9);
+    mo.scale.set(2.2, 1.8);
+    mo.updateHitbox();
+    
+    var dablack:FlxSprite = new FlxSprite(-600, 32, Paths.image('dablack'));
+    dablack.scrollFactor.set(0.9, 0.9);
+    dablack.scale.set(1.9, 1.2);
+    dablack.updateHitbox();
+    
+    var black:FlxSprite = new FlxSprite(-600, 32, Paths.image('black'));
+    black.scrollFactor.set(0.9, 0.9);
+    black.scale.set(3.6, 3.2);
+    black.updateHitbox();
+    
+    if (!Options.lowMemoryMode) {
+        var JGGHD:FlxSprite = new FlxSprite(400, 100);
+        JGGHD.frames = Paths.getSparrowAtlas('stages/mo/JGGHD');
+        JGGHD.animation.addByPrefix('idle', 'idle', 24, true);
+        JGGHD.animation.play('idle', false);
+        JGGHD.scale.set(3.6, 3.2);
+        JGGHD.updateHitbox();
+    }
+    
+    insert(1, moe);
+    insert(2, mo);
+    if (!Options.lowMemoryMode) insert(3, JGGHD);
+    add(dablack);
+    add(black);
+}
