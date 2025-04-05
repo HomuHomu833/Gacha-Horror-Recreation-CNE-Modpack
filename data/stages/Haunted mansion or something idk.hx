4e70a7ba264a9859711c0152a8d00f68503880f3
Solex <95106797+SantiYea@users.noreply.github.com>
2025-04-05 04:24:08 -0300

    me dumb part 5

--- data/stages/Haunted mansion or something idk.hx
+++ data/stages/Haunted mansion or something idk.hx
@@ -8,18 +8,18 @@ function create() {
     gf.setPosition(500, 70);
     dad.setPosition(190, 100);
     
-    bg = new FlxSprite(-300, -70, Paths.images('stages/Creepy Cutie shinaniganz/bg'));
+    bg = new FlxSprite(-300, -70, Paths.image('stages/Creepy Cutie shinaniganz/bg'));
     bg.scrollFactor.set(0.9, 0.9);
     bg.scale.set(1.8, 1.6);
     bg.updateHitbox();
     
     if (!Options.lowMemoryMode) {
-        tre = new FlxSprite(-400, -90, Paths.images('stages/Creepy Cutie shinaniganz/tre'));
+        tre = new FlxSprite(-400, -90, Paths.image('stages/Creepy Cutie shinaniganz/tre'));
         tre.scrollFactor.set(1.5, 1.5);
         tre.scale.set(1.6, 2.3);
         tre.updateHitbox();
         
-        tre2 = new FlxSprite(900, -90, Paths.images('stages/Creepy Cutie shinaniganz/tre2'));
+        tre2 = new FlxSprite(900, -90, Paths.image('stages/Creepy Cutie shinaniganz/tre2'));
         tre2.scrollFactor.set(1.5, 1.5);
         tre2.scale.set(1.6, 2.3);
         tre2.updateHitbox();
